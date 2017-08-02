0fb7d4fb889223beaebfaab9cdac100bd0f8ad92
winder <wwinder.unh@gmail.com>
2017-08-02 09:59:37 -0400

    Add swagger documentation page accessible from 'http://localhost:8080/docs/?url=../swagger.json'

--- src/main/java/org/vahid/resources/SubscriberResource.java
+++ src/main/java/org/vahid/resources/SubscriberResource.java
@@ -1,18 +1,37 @@
 package org.vahid.resources;
 
+import static org.vahid.util.Constants.BOOK_ENTITY;
+import static org.vahid.util.Constants.CATEGORY_ENTITY;
+import static org.vahid.util.Constants.RESOURCE_NOT_OK;
+import static org.vahid.util.Constants.SUBSCRIBER_ENTITY;
+
 import com.google.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.vahid.api.*;
+import org.vahid.api.Book;
+import org.vahid.api.Category;
+import org.vahid.api.FavoriteBooks;
+import org.vahid.api.MapEntity;
+import org.vahid.api.NewsLetter;
+import org.vahid.api.ResponseStatus;
+import org.vahid.api.Subscriber;
 import org.vahid.services.SubscriberService;
-import static org.vahid.util.Constants.*;
 
-import javax.validation.Valid;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * Created by vahid (@vahid_r)
@@ -21,6 +40,7 @@ import java.util.List;
 
 
 @Path("/api/v1")
+@Api(value = "/api/v1", description = "Subscriber endpoints")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class SubscriberResource {
@@ -31,6 +51,9 @@ public class SubscriberResource {
 
     @GET
     @Path("/ping")
+    @ApiOperation(
+        value = "Request a pong.",
+        response = Response.class)
     public Response ping() {
         log.debug("PING called !!");
         return Response.ok().entity("PONG!!").build();
@@ -38,6 +61,9 @@ public class SubscriberResource {
 
     @POST
     @Path("/categories")
+    @ApiOperation(
+        value = "Create a new category",
+        response = Response.class)
     public Response createCategory(@Valid Category category) {
         log.debug("category in Resource: {}", category.toString());
         boolean isSuccessful = subscriberService.createCategory(category);
@@ -50,6 +76,9 @@ public class SubscriberResource {
 
     @POST
     @Path("/books")
+    @ApiOperation(
+        value = "Create a new book",
+        response = Response.class)
     public Response createBook(@Valid Book book) {
         boolean isSuccessful = subscriberService.createBook(book);
         if (isSuccessful) {
@@ -61,6 +90,9 @@ public class SubscriberResource {
 
     @POST
     @Path("/subscribers")
+    @ApiOperation(
+        value = "Create a new subscriber",
+        response = Response.class)
     public Response createSubscriber(@Valid Subscriber subscriber) {
         boolean isSuccessful = subscriberService.createSubscriber(subscriber);
         if (isSuccessful) {
@@ -72,6 +104,9 @@ public class SubscriberResource {
 
     @GET
     @Path("/newsletters")
+    @ApiOperation(
+        value = "Get a newsletter",
+        response = Response.class)
     public Response getNewsLetters() {
         List<NewsLetter> newsLetters = new ArrayList<>();
         List<MapEntity> subscribersWithCats = subscriberService.getSubscribersWithCats();
@@ -85,6 +120,4 @@ public class SubscriberResource {
 
         return Response.ok(newsLetters).build();
     }
-
-
 }
